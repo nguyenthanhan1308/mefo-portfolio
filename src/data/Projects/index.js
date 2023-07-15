ea4987ff58f4eee278afebe6ae4e5c650eeff067
Mona Lisagit <sunthanhan1308@gmail.com>
2023-07-15 20:50:12 +0700

    change project order

--- src/data/Projects/index.js
+++ src/data/Projects/index.js
@@ -1,24 +1,30 @@
-const projectsData = [{
-    url: "https://mefo-blog.vercel.app/",
-    thumb: "blog.png",
-    name: "Personal blog",
-    description: "My personal blog where I write about things I learnt and anything I want to share",
-    tech: ["next", "sanity", "tailwind"],
-    deployment: ["vercel", "heroku"]
-}, {
-    url: "https://mefo-music.vercel.app/",
-    thumb: "music.png",
-    name: "Entertain page",
-    description: "A website for listening to music, reading books and watching stream",
-    tech: ["next", "node", "tailwind"],
-    deployment: ["vercel", "heroku"]
-}, {
-    url: "https://hulu-clone-eight-phi.vercel.app/",
-    thumb: "hulu.png",
-    name: "Hulu clone",
-    description: "Hulu Clone which I followed an online tutorial on Youtube, first website I made using NextJs",
-    tech: ["next", "node", "tailwind"],
-    deployment: ["vercel", "heroku"]
-}
+const projectsData = [
+    {
+        id: 1,
+        url: "https://mefo-music.vercel.app/",
+        thumb: "music.png",
+        name: "Entertain page",
+        description: "A website for listening to music, reading books and watching stream",
+        tech: ["nextjs", "node", "tailwind"],
+        deployment: ["vercel", "heroku"]
+    },
+    {
+        id: 2,
+        url: "https://mefo-blog.vercel.app/",
+        thumb: "blog.png",
+        name: "Personal blog",
+        description: "My personal blog where I write about things I learnt and anything I want to share",
+        tech: ["nextjs", "sanity", "tailwind"],
+        deployment: ["vercel", "heroku"]
+    },
+    {
+        id: 3,
+        url: "https://hulu-clone-eight-phi.vercel.app/",
+        thumb: "hulu.png",
+        name: "Hulu clone",
+        description: "Hulu Clone which I followed an online tutorial on Youtube, first website I made using NextJs",
+        tech: ["nextjs", "node", "tailwind"],
+        deployment: ["vercel", "heroku"]
+    },
 ]
-export {projectsData};
+export { projectsData };
