afb836fa11a97ca3d37a6ba39fba172a114f44bb
Mona Lisagit <sunthanhan1308@gmail.com>
2023-07-15 09:59:26 +0700

    about text-wrap and css

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,14 +3,17 @@ export default function About () {
     return (
         <div id="about" className="about-container display_col center">
             <p className="about-welcome">WELCOME TO MY PORTILLO</p>
-            <h3 className="sub-color">{`Hello, I'm Suns`}</h3>
-            <h2 className="primary-color">A Front-end Developer</h2>
+            <h3 className="sub-color">{`Hello, I'm Nguyen Thanh An`}</h3>
+            <h2 className="primary-color">Front-end Developer</h2>
             <p className="about-me">
-                Fond of optimizing front-end and learning new things.
+                A front-end developer with almost 2 years of experiences, familiar with create user interface using React. Fond of optimizing front-end and learning new things.
             </p>
             <div className="display_row about-nav">
                 <a href="https://github.com/nguyenthanhan1308" target="_blank" rel="noreferrer">
                     <img src="github.png" alt="githubLogo" />
+                </a>
+                <a href="https://www.linkedin.com/in/sunthanhan1308/" target="_blank" rel="noreferrer">
+                    <img src="linkedin.png" alt="linkedinLogo" />
                 </a>
             </div>
         </div>
