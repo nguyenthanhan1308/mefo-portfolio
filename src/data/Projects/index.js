9938e4ae7d9be85e5aee3fbc1c42f09c8f84adba
Mona Lisagit <sunthanhan1308@gmail.com>
2023-08-24 19:15:33 +0700

    [24/8] - change projects display order

--- src/data/Projects/index.js
+++ src/data/Projects/index.js
@@ -1,20 +1,20 @@
 const projectsData = [
     {
         id: 1,
-        url: "https://mefo-music.vercel.app/",
-        thumb: "music.png",
-        name: "Entertain page",
-        description: "A website for listening to music, reading books and watching stream",
-        tech: ["nextjs", "node", "tailwind"],
-        deployment: ["vercel", "heroku"]
-    },
-    {
-        id: 2,
         url: "https://mefo-blog.vercel.app/",
         thumb: "blog.png",
         name: "Personal blog",
         description: "My personal blog where I write about things I learnt and anything I want to share",
         tech: ["nextjs", "sanity", "tailwind"],
+        deployment: ["vercel", "heroku"]
+    },
+    {
+        id: 2,
+        url: "https://mefo-music.vercel.app/",
+        thumb: "music.png",
+        name: "Entertain page",
+        description: "A website for listening to music, reading books and watching stream",
+        tech: ["nextjs", "node", "tailwind"],
         deployment: ["vercel", "heroku"]
     },
     {
